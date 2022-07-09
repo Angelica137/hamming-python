1faf36d36aa50e50ebedd39a5d78b2c4cb8ee3c2
Angelica <Angelica137@users.noreply.github.com>
2022-07-09 20:15:31 +0100

    hamming_distance returns 0 for two equal strings, green

--- scripts/hamming.py
+++ scripts/hamming.py
@@ -2,5 +2,5 @@ def hamming_distance(a: str, b: str) -> int:
     """
     Calculates and returns the number of differences in two DNA strands
     """
-    if a == a:
+    if a == b:
         return 0
