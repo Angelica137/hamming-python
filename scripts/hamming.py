515ebd153c6aadbc21f5d40d01e6b5c37770e8bb
Angelica <Angelica137@users.noreply.github.com>
2022-07-09 20:27:13 +0100

    refactor

--- scripts/hamming.py
+++ scripts/hamming.py
@@ -2,11 +2,8 @@ def hamming_distance(a: str, b: str) -> int:
     """
     Calculates and returns the number of differences in two DNA strands
     """
-    if a == b:
-        return 0
-    else:
-        count = 0
-        for i in range(len(a)):
-            if a[i] != b[i]:
-                count += 1
+    count = 0
+    for i in range(len(a)):
+        if a[i] != b[i]:
+            count += 1
     return count
