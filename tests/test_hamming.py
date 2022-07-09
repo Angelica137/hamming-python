a5fdac9ed334cc5432a8621bbcd97d87b4493f5b
Angelica <Angelica137@users.noreply.github.com>
2022-07-09 20:17:02 +0100

    hamming_distance returns 0 for two equal strings test 2, green

--- tests/test_hamming.py
+++ tests/test_hamming.py
@@ -3,3 +3,7 @@ from scripts.hamming import hamming_distance
 
 def test_hamming_distance_empty_strings():
     assert hamming_distance("", "") == 0
+
+
+def test_hamming_distance_longer_strings():
+    assert hamming_distance("abc", "abc") == 0
