8ed73ea45ce2622b89cbb75baf68ad072351f916
Angelica <Angelica137@users.noreply.github.com>
2022-07-10 18:59:37 +0100

    refactor to use a generator comprehension

--- scripts/hamming.py
+++ scripts/hamming.py
@@ -4,8 +4,4 @@ def hamming_distance(a: str, b: str) -> int:
     """
     if len(a) != len(b):
         raise ValueError("Strands must be of equal length.")
-    count = 0
-    for i in range(len(a)):
-        if a[i] != b[i]:
-            count += 1
-    return count
+    return sum(a[i] != b[i] for i in range(len(a)))
